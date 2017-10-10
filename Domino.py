f22ea2ba31790a98b65993ccfa7ca0743851d4b1
Davi Nascimento de Oliveira <dno@icomp.ufam.edu.br>
2017-10-10 03:09:14 -0400

    Arquivo 'Domino.py' atualizado com mergers dos branches 'dno' e 'fcv' e algumas modificações no código, como adição, edição e remoção de prints, exclusão de comentários, linhas desativadas com comentários e reposicionamento de variáveis e chamadas de funções

--- Domino.py
+++ Domino.py
@@ -1,56 +1,33 @@
 from random import *
 
-pedras = [(0,0),
-        (0,1),(1,1),
-        (0,2),(1,2),(2,2),
-        (0,3),(1,3),(2,3),(3,3),
-        (0,4),(1,4),(2,4),(3,4),(4,4),
-        (0,5),(1,5),(2,5),(3,5),(4,5),(5,5),
-        (0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6)]
+pedras = []
+lados = [] # norte = 0; leste = 1; sul = 2; oeste = 3;
+maos = []
+listaPossiveisJogadas = []
 
+jogada = (0,0,0)
 
-lados = [0,0,0,0] # norte = 0; leste = 1; sul = 2; oeste = 3;
+qtdJogadores = 4
+jogadorAtual = 0
+qtdPassesSeguidos = 0
 
-
-#def removeLinhaPedras(linha):
-#    pedras.remove(pedras[linha])
 
 def adquirirPedra():
-    
     indice = randrange(0,len(pedras))
-    #print "indice -> ", indice, "\n"
     pedra = pedras[indice]
     pedras.pop(indice)
-
-    #if (len(pedras[linha]) == 0):
-   #     removeLinhaPedras(linha)
-
-    print "pedra ->", pedra, "\n"
-    print "Pedras restantes -> ", pedras, "\n"
-
     return pedra
 
 
 def distribuiPedras(qtdJogadores):
-
     return list( map( lambda x: map( lambda x: adquirirPedra(), range(7)), range(qtdJogadores)))
 
 
-qtdJogadores = input()
-
-maos = distribuiPedras(qtdJogadores)
-
-def printPedras():
+def printMaos():
     print "Pedras que cada jogador possui:"
 
     for i in range(qtdJogadores):
         print "    Jogador ", i+1 , " -> ", maos[i]
-
-
-jogadorAtual = 0
-jogada = (6,6,-1)
-listaPossiveisJogadas = [jogada]
-qtdPassesSeguidos = 0
 
 
 def fazJogada(pedra, lado):
@@ -70,6 +47,7 @@ def fazJogada(pedra, lado):
 def possiveisJogadas():
     global listaPossiveisJogadas
     global lados
+
     listaPossiveisJogadas = []
 
     for i in range(4):
@@ -104,7 +82,6 @@ def fazPrimeiraJogada():
             jogadorAtual += 1
 
     x,y,lado = jogada
-    printPartida()
     fazJogada((x,y), lado)
     defineJogadorAtual()
 
@@ -135,7 +112,7 @@ def verificaSeExisteMaoVazia():
 
 
 def printPartida():
-    print "\n___________________________________________________________\nJogador ", jogadorAtual+1, " realiza jogada:\n   Mao -> ", maos[jogadorAtual], "\n   Possiveis jogadas -> ", listaPossiveisJogadas, "\n   Jogada -> ", jogada, "\n\nDados da Partida:\n   Quantidade de passes seguidos -> ", qtdPassesSeguidos, "\n   Lados da mesa -> Norte ==>",lados[0],"\n                    Leste ==>",lados[1],"\n                    Sul   ==>",lados[2],"\n                    Oeste ==>",lados[3]
+    print "\nJogador ", jogadorAtual+1, " realiza jogada:\n   Mao -> ", maos[jogadorAtual], "\n   Possiveis jogadas -> ", listaPossiveisJogadas, "\n   Jogada -> ", jogada, "\n\nDados da Partida:\n   Quantidade de passes seguidos -> ", qtdPassesSeguidos, "\n   Lados da mesa -> Norte ==>",lados[0],"\n                    Leste ==>",lados[1],"\n                    Sul   ==>",lados[2],"\n                    Oeste ==>",lados[3], "\n____________________________________________________________________________________________________________________________"
 
 
 def prepararPartida():
@@ -159,25 +136,32 @@ def prepararPartida():
 
     maos = distribuiPedras(qtdJogadores)
 
+    printMaos()
+
     jogadorAtual = 0
     jogada = (6,6,-1)
     listaPossiveisJogadas = [jogada]
     qtdPassesSeguidos = 0
+    printPartida()
 
 
 def partida():
     global qtdPassesSeguidos
-    partidas = 0
-    totalPasses = 0
-    qtdAcabouPorPasses = 0
+    global jogada
+    numPartida = 1
+    #totalPasses = 0
+    #qtdAcabouPorPasses = 0
+    print "____________________________________________________________________________________________________________________________"
 
-    while partidas < 1000:
+    while numPartida <= 1000:
+        print "|Partida:", numPartida
+        print "|____________________________________________________________________________________________________________________________"
         prepararPartida()
         fazPrimeiraJogada()
         possiveisJogadas()
 
         while (not verificaFimPartida()):
-            printPartida()
+            printMaos()
 
             if len(listaPossiveisJogadas) > 0:
                 escolheJogada()
@@ -187,18 +171,25 @@ def partida():
 
             else:
                 qtdPassesSeguidos += 1
-                totalPasses += 1
-                if qtdPassesSeguidos == qtdJogadores:
-                    qtdAcabouPorPasses += 1
+                #totalPasses += 1
+                jogada = ('-','-','-')
+                #if qtdPassesSeguidos == qtdJogadores:
+                #    qtdAcabouPorPasses += 1
 
+            printPartida()
             defineJogadorAtual()
             possiveisJogadas()
 
-        partidas += 1
+        print "Fim Partida"
+        print "____________________________________________________________________________________________________________________________"
+        print "____________________________________________________________________________________________________________________________"
+        numPartida += 1
 
-    print "\npartidas -> ", partidas
-    print "Total de passes -> ", totalPasses
-    print "Quantidade de vezes que a partida acabou por ninguem ter uma jogada possivel -> ", qtdAcabouPorPasses
+    #print "_______________________________"
+    #print "|Partidas -> ", numPartida-1,"\\________"
+    #print "|Total de passes -> ", totalPasses,"\\_____________________________________________________"
+    #print "|Quantidade de vezes que a partida acabou por ninguem ter uma jogada possivel -> ", qtdAcabouPorPasses,"\\"
+    #print "|________________________________________________________________________________________________________"
 
 
 partida()
